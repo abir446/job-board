893ce1e56f27e2543ab1f2eb9123940058ea4cb5
Abir <abirdutta390@gmail.com>
2024-07-16 21:22:49 +0530

    authentication

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,14 +1,25 @@
 import React from 'react'
 import Link from "next/link";
+import {
+  getSignInUrl,
+  getSignUpUrl,
+  getUser,
+  signOut,
+} from '@workos-inc/authkit-nextjs';
 
-const Header = () => {
+const Header = async() => {
+  const { user } = await getUser();
+  const signInUrl = await getSignInUrl()
+  const signOutUrl = await getSignUpUrl()
   return (
     <header className="">
+      {/* {JSON.stringify(user)} */}
         <div className="flex justify-between items-center mx-auto">
           <Link href={'/'} className="font-bold text-xl">jobBoard.</Link>
           <nav className="flex gap-3 *:py-2 *:px-4 *:rounded-md">
-            <Link href={'/login'} className="bg-gray-200">Login</Link>
-            <Link href={'/new-listing'} className="bg-blue-500 text-white hover:bg-pink-100 transition-all duration-500 hover:text-gray-700">Post a job</Link>
+            {!user && <Link href={signInUrl} className="bg-gray-200">Login</Link>}
+            {user && <form action={async() => {'use server'; await signOut();}}><button className="bg-gray-200 py-2 px-4">Log out</button></form>}
+            {user && <Link href={'/new-listing'} className="bg-blue-500 text-white hover:bg-pink-100 transition-all duration-500 hover:text-gray-700">Post a job</Link>}
           </nav>
         </div>
     </header>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,23 @@
+import Link from 'next/link';
+import {
+  getSignInUrl,
+  getSignUpUrl,
+  getUser,
+  signOut,
+} from '@workos-inc/authkit-nextjs';
 import Hero from "./components/Hero";
 import Jobs from "./components/Jobs";
 
-export default function Home() {
+
+export default async function Home() {
+  // Retrieves the user from the session or returns `null` if no user is signed in
+  const { user } = await getUser();
+
+  // Get the URL to redirect the user to AuthKit to sign in
+  const signInUrl = await getSignInUrl();
+
+  // Get the URL to redirect the user to AuthKit to sign up
+  const signUpUrl = await getSignUpUrl();
   return (
     <>
     <Hero />
